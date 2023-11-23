7d7b9645480126907d7e6adbcfa4f07ea95b7d02
Rosi-07 <116937130+Rosi-07@users.noreply.github.com>
2023-11-22 20:05:29 -0600

    Prueba

---
+++ User.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.Models.Relations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class User : BaseEntity
+    {
+        //User info
+        public int DNI { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string LastName1 { get; set; } = string.Empty;
+
+        public string LastName2 { get; set; } = string.Empty;
+
+        public int PhoneNumber { get; set; }
+
+        public int LicenseUNA { get; set; }
+
+        public bool State { get; set; } = true;
+
+        //Login Info
+        public string Email { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime TokenCreated { get; set; }
+        public DateTime TokenExpires { get; set; }
+
+        //Relations
+
+        public List<User_Role> user_Roles { get; set; }
+
+        public List<Process> Processes { get; set; }
+    }
+}
